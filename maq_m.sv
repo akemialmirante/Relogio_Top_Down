9256eb437a481a1f28bd34175695b6f8aa6f4723
Lyane Araújo <lyane0612@gmail.com>
2025-09-16 22:40:23 -0300

    Update maq_m.sv

--- maq_m.sv
+++ maq_m.sv
@@ -1,10 +1,10 @@
 module maq_m(
 	input logic clk,
 	input logic rst,
-	input logic incrementa_minuto,	// vem do maq_s
-	output logic [3:0] bcd_m_lsd,		// unidade
-	output logic [2:0] bcd_m_msd,		// dezena
-	output logic incrementa_hora		// flag pro maq_h
+	input logic incrementa_minuto,
+	output logic [3:0] bcd_m_lsd,	// unidade
+	output logic [2:0] bcd_m_msd,	// dezena
+	output logic incrementa_hora	// flag
 );
 
 always_ff @(posedge clk or negedge rst) begin
@@ -12,20 +12,17 @@ always_ff @(posedge clk or negedge rst) begin
 		bcd_m_lsd <= 4'd9;
 		bcd_m_msd <= 3'd0;
 	end else begin
-		
-		if(incrementa_minuto)begin //so entra no codigo por conta da flag
-         if (bcd_m_lsd == 9) begin
-			   bcd_m_lsd <= 0;
-		
-			   if(bcd_m_msd == 5) begin
-				   bcd_m_msd <= 0;
-
-			   end else begin
-				   bcd_m_msd <= bcd_m_msd + 1;
-			   end
-		   end else begin
-			   bcd_m_lsd <= bcd_m_lsd + 1;
-		   end		
+		if(incrementa_minuto)begin
+			if (bcd_m_lsd == 9) begin
+				bcd_m_lsd <= 0;
+				if(bcd_m_msd == 5) begin
+					bcd_m_msd <= 0;
+				end else begin
+					bcd_m_msd <= bcd_m_msd + 1;
+				end
+			end else begin
+				bcd_m_lsd <= bcd_m_lsd + 1;
+			end		
 		end
 	end
 end
