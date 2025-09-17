56516839759c38615ec7571eb42d83f162769025
Lyane Araújo <lyane0612@gmail.com>
2025-09-16 22:41:46 -0300

    Update maq_s.sv

--- maq_s.sv
+++ maq_s.sv
@@ -1,9 +1,9 @@
-module maq_s (
-input logic clk,
-input logic rst,
-output logic [3:0] bcd_s_lsd, //unidade
-output logic [2:0] bcd_s_msd, //dezena
-output logic incrementa_minuto //flag
+module maq_s(
+	input logic clk,
+	input logic rst,
+	output logic [3:0] bcd_s_lsd,	//unidade
+	output logic [2:0] bcd_s_msd,	//dezena
+	output logic incrementa_minuto	//flag
 );
 
 always_ff @(posedge clk, negedge rst) begin
@@ -13,10 +13,8 @@ always_ff @(posedge clk, negedge rst) begin
 	end else begin	
 		if (bcd_s_lsd == 9) begin
 			bcd_s_lsd <= 0;
-			
 			if(bcd_s_msd == 5) begin
 				bcd_s_msd <= 0;
-
 			end else begin
 				bcd_s_msd <= bcd_s_msd + 1;
 			end
